a8a09d9cc4e2a3889c9a93d3421cabe761a519f0
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-03 11:50:20 +0530

    Vercel.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePages from "./pages/HomePages"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePages />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
